1ba5272c6d8145d2b30e30f1dd01a637d99344e3
Ömer İslamoğlu <omer@trinsyca.com>
2025-08-07 17:56:26 +0300

    second commit

--- ServoryTutorial/forms/orders.js
+++ ServoryTutorial/forms/orders.js
@@ -50,3 +50,41 @@ function onDataChangeCustomer(oldValue, newValue, event) {
 	return true;
 }
  
+ /**
+ * Fired when the button is clicked.
+ *
+ * @param {JSEvent} event
+ *
+ * @private
+ *
+ * @properties={typeid:24,uuid:"A5752A61-4C97-4FF4-A8F8-60215C221912"}
+ */
+function onActionAddItem(event) {
+	orders_to_order_details.newRecord();
+	orders_to_order_details.quantity = 1;
+}
+ 
+ /**
+ * Called when the columns data is changed.
+ *
+ * @param {Number} foundsetindex
+ * @param {Number} [columnindex]
+ * @param [oldvalue]
+ * @param [newvalue]
+ * @param {JSEvent} [event]
+ * @param {JSRecord} [record]
+ *
+ * @return {Boolean}
+ *
+ * @private
+ *
+ * @properties={typeid:24,uuid:"05397A3A-1C4D-47CE-8C8A-9A52C2655696"}
+ */
+function onColumnDataChange(foundsetindex, columnindex, oldvalue, newvalue, event, record) {
+	if(columnindex == 0){
+		orders_to_order_details.unitprice = 
+			orders_to_order_details.order_details_to_products.unitprice;
+	}
+	return true;
+}
+ 
